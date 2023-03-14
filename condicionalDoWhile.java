0849b4820452515f6b8bf6937bef05735dd713e5
Jose Germán Martínez <jgermanmm@gmail.com>
2023-03-14 13:25:35 -0300

    condicionales y ciclos de iteracion

---
+++ condicionalDoWhile.java
@@ -0,0 +1,12 @@
+public class condicionalDoWhile {
+    
+    public static void main(String[] args) {
+        
+        int i = 0;
+        do {
+            System.out.println("La posición en el ciclo es " + i);
+            i++;
+        } while (i <= 10);
+
+    }
+}
---
+++ condicionalwhile.java
@@ -0,0 +1,15 @@
+public class condicionalwhile { 
+
+        public static void main(String[] args) {
+            int n = 10;
+            int x = 1;
+
+            while (x<= n)
+            {
+                System.out.println(x);
+                x++;
+            }
+
+        }
+
+}
