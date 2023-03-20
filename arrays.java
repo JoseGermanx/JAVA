1ed66663ec2dbf411558bc23c792340d5b5f0180
Jose Germán Martínez <jgermanmm@gmail.com>
2023-03-20 17:03:31 -0300

    praactica de arrays

---
+++ arrays.java
@@ -0,0 +1,16 @@
+public class arrays {
+
+    public static void main(String[] args) {
+
+        int notas[] = {1,2,3,4,5,6,7,8};
+
+        //recorremos el arreglo
+
+        for(int i=0; i<notas.length; i++) {
+
+            System.out.println("Nota: " + notas[i]);
+
+        }
+
+    }
+}
